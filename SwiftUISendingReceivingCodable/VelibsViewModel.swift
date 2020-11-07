2403706be60dbfaad4d78bbbe1c16ddb03a10745
picshertho <picshertho@voila.fr>
2020-11-07 15:37:14 +0100

    add file old velibViewModel

---
+++ SwiftUISendingReceivingCodable/VelibsViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  VelibsViewModel.swift
+//  SwiftUISendingReceivingCodable
+//
+//  Created by picshertho on 19/10/2020.
+//
+
+import Combine
+import SwiftUI
+
+class VelibsViewModel: ObservableObject {
+    
+    private var task: AnyCancellable?
+    @Published var velibs: [Velib] = []
+    @Published var velibSelected = [Velib]()
+    @Published var annotations = [Annotation]()
+    @Published var showingErrorAlert = false
+    @Published var results = [GenericData]()
+    @Published var alertError: Alert?
+
+    //    Version COMBINE
+//    func fetchVelibs() {
+        //        task = URLSession.shared.dataTaskPublisher(for: URL(string: UrlDataLocationEnum.allVelibs.rawValue)!)
+        //            .map { $0.data }
+        //            .decode(type: [Velib].self, decoder: JSONDecoder())
+        //            .replaceError(with: [])
+        //            .eraseToAnyPublisher()
+        //            .receive(on: RunLoop.main)
+        //            .assign(to: \VelibsViewModel.velibs, on: self)
+        //    }
+    
+    func fetchVelibs() {
+        MapServices.shared.loadData(url: UrlDataLocationEnum.allVelibs.rawValue, decodable: ResponseData.self, completion: { decodedResponse, error in
+            if let dataResults = decodedResponse?.records {
+                DispatchQueue.main.async {
+                    self.results = dataResults
+                    print("results \(dataResults.count)")
+                    self.createAnnotations(results: dataResults)
+                }
+            }
+            else {
+                self.showingErrorAlert = true
+                if let error = error?.localizedDescription {
+                    self.alertError = Alert(title: Text("Error"), message: Text(error), dismissButton: .default(Text("OK")) {
+                        self.showingErrorAlert = false
+                    })
+                }
+            }
+        })
+    }
+    
+    fileprivate func createAnnotations(results: [GenericData]) {
+        annotations.removeAll()
+        for annotation in results {
+            DispatchQueue.main.async {
+                self.annotations.append(Annotation(data: annotation))
+            }
+        }
+    }
+    
+
+}
